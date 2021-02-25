c06515631e2d3af03cbf1a5a4a9557be35c262ec
David De Smet <2607383+idaviddesmet@users.noreply.github.com>
2021-02-25 17:44:29 -0600

    Added TailwindCSS addons

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,5 @@ module.exports = {
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/line-clamp"), require("@tailwindcss/typography"), require("@tailwindcss/forms")]
 };
